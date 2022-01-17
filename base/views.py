c1a3d616d00e176d138489230c13f14911d9ab3b
sc0rp10n16 <2010040058@klh.edu.in>
2022-01-17 18:59:28 +0530

    Updated template files

--- base/views.py
+++ base/views.py
@@ -1,10 +1,25 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
+rooms = [
+    {'id': 1, 'name': "Lets Learn Python!"},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend Developers'},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+        
+    return render(request, 'base/room.html', context)
